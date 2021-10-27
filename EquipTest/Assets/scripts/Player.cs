93af9543b24f622edbfa1ca9b115c58546a32b38
이윤석 <59646592+simplex3510@users.noreply.github.com>
2021-10-27 21:25:39 +0900

    애니메이션 공부하기

---
+++ EquipTest/Assets/scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject m_obj2HandSword;
+    public GameObject m_objCurrent;
+    public Animator m_animator;
+
+    public void Click_unarmed()
+    {
+        if(m_objCurrent)
+        {
+            m_objCurrent.SetActive(false);
+        }
+        m_objCurrent = null;
+
+        m_animator.SetTrigger("equip");
+        m_animator.SetInteger("weapon", 0); // enum 설정 필요.
+    }
+    public void Click_2HandSword()
+    {
+        if (m_objCurrent)
+        {
+            m_objCurrent.SetActive(false);
+        }
+
+        m_objCurrent = m_obj2HandSword;
+
+        m_animator.SetTrigger("equip");
+        m_animator.SetInteger("weapon", 1); // enum 설정 필요.
+    }
+
+
+    public void WeaponSwitch()
+    {
+        Debug.Log(" WeaponSwitch()  !!!");
+        if(m_obj2HandSword)
+        {
+            m_obj2HandSword.SetActive(true);
+        }
+
+    }
+}
